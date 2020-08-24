9f9f2afc9ac93a209cb749fb32c42bc2e7e1d8f0
tonky0110 <tonky0110@gmail.com>
2020-08-24 23:51:25 +0900

    first express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+const handleListening = () => {
+  console.log(`Listening in port on: http://localhost:${PORT}.`);
+};
+
+app.listen(PORT, handleListening);
