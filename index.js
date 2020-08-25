82106a65d558fb906ece2399c33e7a17048511e4
tonky0110 <tonky0110@gmail.com>
2020-08-25 23:59:00 +0900

    2.5.3 babel업데이트로 인한 문제가 있지만, 지금 프로젝트는 아님

--- index.js
+++ index.js
@@ -7,15 +7,8 @@ const handleListening = () => {
   console.log(`Listening in port on: http://localhost:${PORT}.`);
 };
 
-function handleHome(req, res) {
-  console.log("req: ", req);
-  res.send("Hello from home.");
-}
-
-function handleProfile(req, res) {
-  console.log("req: ", req);
-  res.send("You are on my profile.");
-}
+const handleHome = (req, res) => res.send("Hello from home.");
+const handleProfile = (req, res) => res.send("You are on my profile.");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
