1f6bb5893acb2074db27a9a979fa0c5c04c1485d
tonky0110 <tonky0110@gmail.com>
2020-08-25 23:31:59 +0900

    2.4 EXPRESS서버를 이용한 get메소드 처리 simple example.

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ const handleListening = () => {
   console.log(`Listening in port on: http://localhost:${PORT}.`);
 };
 
+function handleHome(req, res) {
+  console.log("req: ", req);
+  res.send("Hello from home.");
+}
+
+function handleProfile(req, res) {
+  console.log("req: ", req);
+  res.send("You are on my profile.");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
