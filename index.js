7a7afaf573baeee74d38e8bd341e9f36aeb73704
tonky0110 <tonky0110@gmail.com>
2020-08-28 21:35:57 +0900

    morgan - 로깅 처리를 위한 라이브러리. helmet - 보안관련 라이브러리. body-parser - form, post로 전달되는 파라미터를 파싱처리 cookie-parser - cookie를 파싱 처리

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -18,7 +22,11 @@ const betweenHome = (req, res, next) => {
 };
 
 // use middleware
-app.use(betweenHome); // 전역으로 전체 route에서 적용.
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 // app.get("/", betweenHome, handleHome); // 필요한 routes에서 선택적으로 적용.
 
 // routes
