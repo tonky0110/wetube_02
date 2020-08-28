1da0302331771f4bda2768e4a84c37251716c27a
tonky0110 <tonky0110@gmail.com>
2020-08-28 21:26:31 +0900

    2.6 middleware와 적용 위치 설정.

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ const handleListening = () => {
   console.log(`Listening in port on: http://localhost:${PORT}.`);
 };
 
+// event handler
 const handleHome = (req, res) => res.send("Hello from home.");
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
+// middleware
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+// use middleware
+app.use(betweenHome); // 전역으로 전체 route에서 적용.
+// app.get("/", betweenHome, handleHome); // 필요한 routes에서 선택적으로 적용.
+
+// routes
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
