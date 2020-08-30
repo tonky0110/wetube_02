6c37513a98f6573e078b7117282be289043dcd56
tonky0110 <tonky0110@gmail.com>
2020-08-30 13:50:36 +0900

    controller 분리

---
+++ controllers/videoController.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+
+export const videos = (req, res) => res.send("Video index"));
+export const upload = (req, res) => res.send("Video update"));
+export const videoDetail = (req, res) => res.send("Video Detail"));
+export const editVideo = (req, res) => res.send("Edit Video"));
+export const deleteVideo = (req, res) => res.send("Delete Video"));
