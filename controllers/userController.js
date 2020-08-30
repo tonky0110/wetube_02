7286052b90a38c1059b45ef6410c35116c676f1c
tonky0110 <tonky0110@gmail.com>
2020-08-30 21:51:49 +0900

    2.18 search term send to backend and process controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
-export const join = (req, res) => res.send("Join", { pageTitle: "Join" });
-export const login = (req, res) => res.send("Login", { pageTitle: "Login" });
-export const logout = (req, res) => res.send("Logout", { pageTitle: "Logout" });
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
 
-export const users = (req, res) =>
-  res.send("user index", { pageTitle: "Users" });
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
-  res.send("User Detail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.send("Edit Profile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
-  res.send("Change Password", { pageTitle: "Change Password" });
+  res.render("changePassword", { pageTitle: "Change Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,16 @@
-export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
-export const search = (req, res) => res.send("Search", { pageTitle: "Search" });
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  console.log("req.query: ", req.query);
+  const { query: { term: searchingBy = "" } = {} } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 export const videos = (req, res) =>
-  res.send("Video index", { pageTitle: "Videos" });
+  res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
-  res.send("Video update", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
-  res.send("Video Detail", { pageTitle: "Video Detail" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
-  res.send("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
-  res.send("Delete Video", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
