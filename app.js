e1f16382794b161ec324790b8af7b686a6cde0af
tonky0110 <tonky0110@gmail.com>
2020-08-30 20:43:43 +0900

    2.16 LOCAL VALIABLES in pug

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-import routes from "./routes";
 
 const app = express();
 
@@ -25,11 +25,12 @@ const betweenHome = (req, res, next) => {
 app.set("view engine", "pug");
 
 // use middleware
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 // app.get("/", betweenHome, handleHome); // 필요한 routes에서 선택적으로 적용.
 
 // routes
