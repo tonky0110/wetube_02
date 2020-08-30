f9cf16b8d446d6157e7986707cc03922381304e9
tonky0110 <tonky0110@gmail.com>
2020-08-30 16:28:25 +0900

    2.14 Template Extends main 뼈대는 그대로 두고, 필요한 block을 만들어 둔 후 해당 영역만 extends해서 확장하여 사용한다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
 export const home = (req, res) => res.render("Home");
 export const search = (req, res) => res.send("Search");
 
-export const videos = (req, res) => res.send("Video index"));
-export const upload = (req, res) => res.send("Video update"));
-export const videoDetail = (req, res) => res.send("Video Detail"));
-export const editVideo = (req, res) => res.send("Edit Video"));
-export const deleteVideo = (req, res) => res.send("Delete Video"));
+export const videos = (req, res) => res.send("Video index");
+export const upload = (req, res) => res.send("Video update");
+export const videoDetail = (req, res) => res.send("Video Detail");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
